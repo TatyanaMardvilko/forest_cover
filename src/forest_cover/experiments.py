8a7781cd1e19e6e20ae5d90d54cb53cc463154d1
Tatyana Mardvilko <mardvilko@gmail.com>
2022-05-09 22:30:18 +0300

    add logistic regression with research grid

--- src/forest_cover/experiments.py
+++ src/forest_cover/experiments.py
@@ -1,8 +1,15 @@
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
 models = []
 
 models.append("Logistic Regression:", LogisticRegression())
 models.append("Random Forest:", RandomForestClassifier())
+
+param_grid = {'C': np.logspace(-4, 4, 50), 'penalty': ['l1', 'l2']}
+
+models.append("Logistic Regression with GridSearch:",
+              GridSearchCV(LogisticRegression(random_state=0), param_grid, cv =5, verbose = 0, n_jobs = -1))
